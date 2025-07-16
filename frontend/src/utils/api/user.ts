f8a0584bbe92c16bcde88c53c2dd4ce34e71a201
Pranavlovescode <pranavtitambe04@gmail.com>
2025-07-16 21:30:17 +0530

    fixed the issue

--- frontend/src/utils/api/user.ts
+++ frontend/src/utils/api/user.ts
@@ -16,7 +16,6 @@ export const UserApi = {
           headers: {
             "Content-Type": "application/json",
           },
-          withCredentials: true,
         }
       );
       return response.data;
